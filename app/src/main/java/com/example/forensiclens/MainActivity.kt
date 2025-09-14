b8bfc94c065da18c81cb22b4b57c601e1181b0e6
nervachan <nervagrf@gmail.com>
2025-09-14 22:51:03 +0800

    finished landing page and login page

---
+++ app/src/main/java/com/example/forensiclens/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.forensiclens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.forensiclens.ui.theme.ForensicLensTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
